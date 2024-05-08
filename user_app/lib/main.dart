83dde0cc695dfd6ddecb0cba1e5d674d1c3f4e03
mr.mojtaba <mr.mojtaba47@gmail.com>
2024-05-08 23:44:54 +0330

    feat : init firebase

--- user_app/lib/main.dart
+++ user_app/lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:user_app/presentation/ui/pages/auth/sign_up/sign_up_page.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
